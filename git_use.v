c21dba064fb2a20f09cce7eabaff14446884675d
ruifree <wanqiangwu@qq.com>
2020-10-11 17:46:19 +0800

    git_use.v首次提交

---
+++ git_use.v
@@ -0,0 +1,13 @@
+1、本地删除了文件，且与远程同步
+git status // 非必须
+git commit -am '提交注释'
+git push origin master
+
+2、新增文件，且与远程同步
+git add 文件名
+git commit -m '提交注释'
+git push origin master
+
+3、从远程拉取文件
+git pull origin master
+
